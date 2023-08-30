cbff4807d49b425b217151d7e5f5706222a327f5
unknown <ogbondafavour9@gmail.com>
2023-08-30 11:40:04 +0100

    feat: created the landing page

--- app/page.js
+++ app/page.js
@@ -1,25 +1,51 @@
 import Image from 'next/image'
 import styles from './page.module.css'
+import cardImage from '../public/creditCard.jpg'
 
 export default function home() {
   return (
     <>
-      <div className={styles.main}>
-       <ul className={styles.menu}>
-        <li className={styles.menuItem}>
-          <p>Home</p>
-        </li>
-        <li className={styles.menuItem}>
-          <p>Services</p>
-        </li>
-        <li className={styles.menuItem}>
-          <p>Checkout</p>
-        </li>
-        <li className={styles.menuItem}>
-          <p>Contact Us</p>
-        </li>
-       </ul>
+      <div className={styles.body}>
+        <div className={styles.header}>
+          <div className={styles.headerTitle}>
+            <h2>Pay - Go</h2>
+          </div>
+
+          <div className={styles.mainMenu}>
+            <ul className={styles.menu}>
+              <li className={styles.menuItem}>
+                <p>Home</p>
+              </li>
+              <li className={styles.menuItem}>
+                <p>Services</p>
+              </li>
+              <li className={styles.menuItem}>
+                <p>Checkout</p>
+              </li>
+              <li className={styles.menuItem}>
+                <p>Contact Us</p>
+              </li>
+            </ul>
+          </div>
+
+        </div>
+
+        <div className={styles.mainBody}>
+          <div className={styles.contentOne}>
+            <h1>
+              Make Payments
+            </h1>
+            <p>
+              Discover a New Era of Payments with Pay-Go! Experience effortless transactions through our intuitive interface, backed by state-of-the-art security protocols that keep your financial information safe. Enjoy instant processing, tailored solutions for your business, and the ability to connect with customers worldwide. Our round-the-clock support is here for you. Embrace the future of seamless and secure payments – join Pay-Go today!
+            </p>
+            <button>Click to Pay</button>
+          </div>
+          <div className={styles.contentTwo}>
+            <Image src={cardImage} alt="payment image" loading="lazy" placeholder="blur"/>
+          </div>
+        </div>
       </div>
+      
     </>
   )
 }
